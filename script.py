474b21a2d5e39de88b93b7dc3e2eb6bf1470597f
rnw10va <rnw10va@gmail.com>
2024-06-29 11:14:59 -0600

    Updated script.py to remove duplicates that appear in some lists' code

--- script.py
+++ script.py
@@ -4,5 +4,7 @@ backloggdWebPage="""
 
 """
 gamesListWithExtraData=re.findall("\t<a href=\"/games/[a-z0-9\-]+/\">",backloggdWebPage)
+listOfGames=[]
 for gameWithExtraData in gamesListWithExtraData:
-   print(gameWithExtraData.split("\t<a href=\"/games/")[1].split("/\">")[0].replace("-"," "))
+   listOfGames.append(gameWithExtraData.split("\t<a href=\"/games/")[1].split("/\">")[0].replace("-"," "))
+print(*set(listOfGames),sep="\n")
